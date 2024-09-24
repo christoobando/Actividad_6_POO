07d4fa05f55dd8fdb831b18063346f4cd0492386
Christopher <Christopher@DESKTOP-6E1TUBO>
2024-09-24 18:08:59 -0500

    Actividad 6

---
+++ src/crud/DisplayFriends.java
@@ -0,0 +1,40 @@
+package crud;
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisplayFriends {
+
+    public static String[] displayFriends() {
+        try {
+            String nameNumberString;
+            String name;
+            long number;
+            List<String> friendList = new ArrayList<>();
+
+            File file = new File("contactosAmigos.txt");
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            RandomAccessFile raf = new RandomAccessFile(file, "rw");
+
+            while (raf.getFilePointer() < raf.length()) {
+                nameNumberString = raf.readLine();
+                String[] lineSplit = nameNumberString.split("!");
+                name = lineSplit[0];
+                number = Long.parseLong(lineSplit[1]);
+                friendList.add(name + ": " + number);
+            }
+
+            raf.close();
+            return friendList.toArray(new String[friendList.size()]);
+        } catch (Exception e) {
+            String[] error = { e.getMessage() };
+            return error;
+        }
+    }
+}
